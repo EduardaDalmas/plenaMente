9a5a6969794a700a96a36c0e20ed78a4e7513c94
DalmasEduarda <eduarda.dalmas@doctorclin.com.br>
2023-05-08 11:12:13 -0300

    ajuste card

---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-card class="card">
+    <div class="row">
+      <q-icon class="icone" :name="icone"  v-bind:style="{ 'color': cor }"></q-icon>
+    </div>
+    <div class="row">
+      <q-chip  v-bind:style="{ 'background-color': cor }">{{ texto }}</q-chip>
+    </div>
+  </q-card>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CardComponent',
+  props: {
+    texto: {
+      type: String,
+      required: true
+    },
+
+    icone: {
+      type: String,
+      default: '#'
+    },
+
+    cor: {
+      type: String,
+      default: '#'
+    },
+  }
+})
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,52 @@
 <template>
-  <q-page class="flex flex-center">
-    <h3>Como você está se sentindo hoje?</h3>
+  <q-page class="container">
+    <div class="text-center">
+      <h4>Como você está se sentindo hoje?</h4>
+    </div>
+    <div class="row">
+      <div v-for="item in sentimentos" :key="item" class="col-md-4">
+        <Card :icone="item.icone" :texto="item.texto" :cor="item.cor"></Card>
+      </div>
+    </div>
+
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import Card from '../components/Card.vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  components: {
+    Card
+  },
+
+  setup() {
+    let sentimentos = [
+      {
+        icone: 'sentiment_satisfied',
+        texto: 'Feliz',
+        cor: '#6BC24C'
+      },
+      {
+        icone: 'sentiment_neutral',
+        texto: 'Indiferente',
+        cor: '#F5A429'
+      },
+      {
+        icone: 'sentiment_dissatisfied',
+        texto: 'Triste',
+        cor: '#69CEC8'
+      }
+    ]
+
+    return {
+      sentimentos
+    }
+  }
+
+
 })
 </script>
